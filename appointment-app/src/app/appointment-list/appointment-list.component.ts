6f9875a0b54f6cddc6cb4311f79f242769bdf4fd
GieHan <Gie.Hanggoro@gmail.com>
2024-03-29 11:10:54 +0100

    Add local store capability

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
 
   newAppointmentTitle : string  = "";
   newAppointmentDate : Date     = new Date();
 
   appointments: Appointment[]   = [];
+
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+    this.appointments     = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   addAppointment(){
     // Check for empty input first
@@ -28,9 +34,18 @@ export class AppointmentListComponent {
     //Refresh user input to default, since it is two way data binding.
     this.newAppointmentTitle = "";
     this.newAppointmentDate = new Date();
+
+    this.saveLocaly();
   }
 
   deleteAppointment(index: number){
     this.appointments.splice(index, 1);
+
+    this.saveLocaly();
+  }
+
+  // Save/update all current appointments into local storage (simulate database)
+  saveLocaly(){
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 }
