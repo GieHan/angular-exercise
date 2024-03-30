3e1f9e7e71fead377ad192d323c1dedb2b166817
GieHan <Gie.Hanggoro@gmail.com>
2024-03-30 10:38:58 +0100

    Add Forms and validation (reactive form)

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit{
 
+    reservationForm: FormGroup = new FormGroup({});
+
+    constructor(private formBuilder: FormBuilder){
+
+    }
+
+    ngOnInit(): void {
+      // The validators on html need to be add as group
+      this.reservationForm = this.formBuilder.group({
+        checkInDate: ['', Validators.required],
+        checkOutDate: ['', Validators.required],
+        guestName: ['', Validators.required],
+        guestEmail: ['', Validators.compose([Validators.required, Validators.email])], //Use Validator compose for multiple validator (otherwise error)
+        roomNumber: ['', Validators.required]
+      })
+    }
+
+    onSubmit(){
+      if (this.reservationForm.valid) {
+        console.log('valid');
+      }
+    }
 }
